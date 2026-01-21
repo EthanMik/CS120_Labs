d938ec7dc5a895dc06ea6a7b9f5cf8701175054f
EthanMik <mikolaycikethan@gmail.com>
2026-01-21 13:02:57 -0600

    Updated CS120 Labs to Linux, using g++13

---
+++ out/build/Clang 18.1.3 x86_64-pc-linux-gnu/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1.dir/lab1/foo.cpp.o"
+  "CMakeFiles/lab1.dir/lab1/foo.cpp.o.d"
+  "CMakeFiles/lab1.dir/lab1/main.cpp.o"
+  "CMakeFiles/lab1.dir/lab1/main.cpp.o.d"
+  "bin/lab1"
+  "bin/lab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ out/build/Clang 18.1.3 x86_64-pc-linux-gnu/CMakeFiles/lab2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab2.dir/lab2/main.cpp.o"
+  "CMakeFiles/lab2.dir/lab2/main.cpp.o.d"
+  "bin/lab2"
+  "bin/lab2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
